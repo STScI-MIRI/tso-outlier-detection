d3e57aabb8884d2b8a65466a142d12d42e2c67f8
Sarah Kendrew <sarahaskendrew@gmail.com>
2021-01-27 16:03:27 -0500

    started on clipping function

--- tso_outlier_detection.py
+++ tso_outlier_detection.py
@@ -14,7 +14,7 @@ import pdb
 
 # this function implements the outlier detection algorithm for TSOs with JWST
 
-def outlier_det(model, ndiff=2, box_size=10, sigma=5.0, verbose=False):
+def make_diff_im(model, ndiff=2, box_size=10, sigma=5.0, verbose=False):
 	
 	''' This function implements the median difference image algorithm on Webb time series observations for detection of outliers. The algorithm follows broadly the method of Nikolov et al 2014 developed for HST.
 	
@@ -84,7 +84,7 @@ def outlier_det(model, ndiff=2, box_size=10, sigma=5.0, verbose=False):
 		if len(dd) < 3:
 			flags[i] = 0
 		
-		# conver the list into a 3-D array
+		# convert the list into a 3-D array and take a median along the first axis
 		tmp_arr = np.asarray(dd)
 		diff_ims[i,:,:] = np.median(tmp_arr, axis=0)
 		
@@ -93,7 +93,7 @@ def outlier_det(model, ndiff=2, box_size=10, sigma=5.0, verbose=False):
 	fig, ax = plt.subplots(ncols=10, nrows=1, figsize=[20,8])
 	
 	for i in range(nint):
-		ax[i].imshow(diff_ims[i,:,:], origin='lower', interpolation='None')
+		ax[i].imshow(diff_ims[i,:,:], origin='lower', interpolation='None', cmap='gray')
 	
 	fig.show()
 	
@@ -102,4 +102,62 @@ def outlier_det(model, ndiff=2, box_size=10, sigma=5.0, verbose=False):
 	return diff_ims
 	
 	
+
+def find_outliers(im, dq=None, sigma=4.0, mode='box', box_size=None, dispersion_axis=None, verbose=False, plot=False):
+	
+	'''
+	This function will find outliers in image im
+	
+	Parameters
+	----------
+	- im:		the input image. must be a 2D array of floats.
+	- dq:		an array of data quality flags (the DQ array) (optional). If no DQ array is provided the algorithm will make no use of existing flags.
+	- mode:		the spatial method used for outlier detection. options are: 
+					* 'box' (default): pixel value will be compared with a square box of box_size width x length
+					* 'line': pixel will be compared against the full pixel row/column, with direction defined to be perpendicular to the dispersion_axis.
+	- box_size:	the size of the box to which each pixel will be compared (integer; required if mode='box'). 
+	- dispersion_axis:	the direction along which the spectrum is dispersed. this should correspond to the axis in the input array. (integer; required if mode='line')
+	- verbose:	for added text output (boolean; default=False)
+	- plot:		for visual output of outlier pixels (bolean; default=False) 
+	- 
+	'''
+	
+	# change number formats if needed
+	if box_size:
+		if not isinstance(box_size, int):
+			box_size = np.int(np.round(box_size))
+			print('Converting box size to integer value: {0}'.format(box_size))
+	
+	if dispersion_axis:
+		if not isinstance(dispersion_axis, int):
+			dispersion_axis = np.int(np.round(dispersion_axis))
+			print('Converting dispersion axis to integer value: {0}'.format(dispersion_axis))
+	
+	# checks to start:
+	# - input image is a 2D array
+	# - if a DQ array is provided, that the array sizes match
+	# - if mode is box, that a box size is given and box size is smaller than the smallest array dimension
+	# - if mode is line, that the dispersion_axis is given
+	# - that the dispersion axis is 0 or 1
+	assert(np.ndim(im)) == 2, "Input image is not a 2D array"
+	if dq:
+		assert(np.shape(im) == np.shape(dq)), "Data and DQ array shapes do not match"
+	if (mode == 'box'):
+		assert box_size is not None, "You must specify a box size with mode 'box' "
+		assert box_size <= np.min(np.shape(im))
+	if (mode == 'line'):
+		assert dispersion_axis is not None, "You must specify a dispersion axis with mode 'line' "
+		assert dispersion_axis < 2, "Dispersion axis not compatible with array shape"
+		assert dispersion_axis > 0, "Dispersion axis cannot be negative"
+		
+	
+	return im
+	
+	
+	# if plot == True, set up the output image
+	
+	
+	
+	
+	# 
 	
